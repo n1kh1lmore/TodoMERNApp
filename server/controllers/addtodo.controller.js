56262325f475311ca1e0b4f708a3725478aaed28
Subin <subinsk284@gmail.com>
2021-09-24 00:54:51 +0530

    init

---
+++ server/controllers/addtodo.controller.js
@@ -0,0 +1,10 @@
+const Todo = ('../models/todo.model');
+
+const todo = new Todo(req.body);
+    todo.save()
+        .then( todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch( err => {
+            res.status(400).send('adding new todo failed');
+        });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+app.get('/',(req,res)=>{
+    res.status(200).send('Success');
+});
+
 app.listen( PORT, () => {
     console.log("Server is running on port " + PORT);
 })
