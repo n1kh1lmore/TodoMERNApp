db39e25807e15fcdedeff65bc8091311f1c38920
Subin <subinsk284@gmail.com>
2021-09-24 11:03:28 +0530

    day 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import CreateTodo from './components/CreateTodo';
+import logo from './logo.svg';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello</h1>
-            </div>
+            <Router>
+                <div className="container">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <a className="navbar-brand" href="/">
+                            <img src={logo} width="30" height="30" alt="logo" />
+                        </a>
+                        <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="myNavbar">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="navbar-item">
+                                    <Link to="/" className="nav-link">Todos</Link>
+                                </li>
+                                <li className="navbar-item">
+                                    <Link to="/create" className="nav-link">Create Todo</Link>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </nav>
+                    <Route path='/create' component={CreateTodo} />
+                </div>
+            </Router>
         )
     }
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,54 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const PORT = 4000;
+const mongoose = require('mongoose')
+const Todo = require('./models/todo.model')
 
-app.get('/',(req,res)=>{
-    res.status(200).send('Success');
+const app = express();
+const router=express.Router();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/todos', { useNewUrlParser: true ,useUnifiedTopology: true});
+const connection = mongoose.connection;
+
+// Once the connection is established, callback
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
 });
+
+router.get('/', (req,res) => {
+    Todo.find((err, todos) => {
+        if(err)
+            console.log(err);
+        else {
+            res.json(todos);
+        }
+    });
+});
+
+router.route('/:id').get((req,res) => {
+    const id = req.params.id;
+    Todo.findById(id, (err,todo) => {
+        res.json(todo);
+    });
+});
+
+router.route('/add').post((req,res) => {
+    const todo = new Todo(req.body);
+    todo.save()
+        .then( todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch( err => {
+            res.status(400).send('adding new todo failed');
+        });
+});
+
+app.use('/todos', router);
 
 app.listen( PORT, () => {
     console.log("Server is running on port " + PORT);
